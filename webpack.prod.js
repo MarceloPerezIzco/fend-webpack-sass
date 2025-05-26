bec5e6757cdd35a3168939c6027075a35b77b514
Marcelo Perez Izco <marceloperezizco@gmail.com>
2025-05-26 16:24:34 +0100

    Configure production build with MiniCssExtractPlugin, TerserPlugin, and CssMinimizerPlugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,15 @@
 const path = require("path");
-const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
     entry: "./src/client/index.js",
     mode: "production",
     output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
         libraryTarget: "var",
         library: "Client",
     },
@@ -18,14 +22,21 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: ["style-loader", "css-loader", "sass-loader"],
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
             },
         ],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
     },
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
+        }),
+        new MiniCssExtractPlugin({
+            filename: "main.css",
         }),
     ],
 };
